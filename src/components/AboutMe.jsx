3b5ec4a2b7955c16eda662d1865e45a38e8d6730
notacoham <alexcottam12@gmail.com>
2026-03-30 00:18:42 -0600

    update AboutMe, Experience, and Projects components with new roles, skills, and project details

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,72 +1,73 @@
-import profileImg from '../assets/profile-img.png';
+import profileImg from "../assets/profile-img.png";
 
 const AboutMe = () => {
-    return (
-        <div className="p-4">
-            <div className="flex flex-col sm:flex-row items-center sm:items-start">
-                <img
-                    src={profileImg}
-                    alt="profile image"
-                    className="w-30 h-30 rounded-full hover:scale-110 transition-transform duration-200 mb-4 sm:mb-0 flex-shrink-0"
-                />
-                <div className="sm:ml-6 text-center sm:text-left">
-                    <h1 className="font-bold text-2xl sm:text-3xl mb-2">Alex Cottam</h1>
-                    <p className="text-lg">
-                        iOS Developer @{" "}
-                        <a className="text-amber-400 underline" target='_blank' href="https://www.amanocare.com/">
-                            Amano Technologies
-                        </a>
-                    </p>
-                    <p className="text-lg">
-                        Web Development Instructor @{" "} <a className="text-amber-400 underline" target='_blank' href="https://slcc.edu">SLCC</a>
-                    </p>
-                </div>
-            </div>
-            <hr className="border-t border-gray-300 my-4" />
-            <div className="mt-4">
-                <p className="text-lg mb-2 font-semibold">
-                    Hi! I'm Alex, an iOS and Web Developer. I...
-                </p>
-                <ul className="list-disc list-inside mb-2">
-                    <li className="text-sm">
-                        Create user-friendly and performant mobile applications and websites
-                    </li>
-                    <li className="text-sm">
-                        Develop and maintain RESTful APIs in Django and Node
-                    </li>
-                    <li className="text-sm">
-                        Work with Google Cloud and Firebase
-                    </li>
-                    <li className="text-sm">
-                        Teach web development concepts to students at SLCC in
-                        evening classes
-                    </li>
-                </ul>
-            </div>
-            <div className="mt-4">
-                <p className="text-lg mb-2 font-semibold">
-                    Other interests:
-                </p>
-                <ul className="list-disc list-inside mb-2">
-                    <li className="text-sm">
-                        Watch and play hockey (Go Mammoths!)
-                    </li>
-                    <li className="text-sm">
-                        Backend development with Python
-                    </li>
-                    <li className="text-sm">
-                        Cook and eat delicious food from all cultures
-                    </li>
-                    <li className="text-sm">
-                        Read sci-fi and fantasy novels
-                    </li>
-                    <li className="text-sm">
-                        Playing video games (especially Nintendo)
-                    </li>
-                </ul>
-            </div>
+  return (
+    <div className="p-4">
+      <div className="flex flex-col sm:flex-row items-center sm:items-start">
+        <img
+          src={profileImg}
+          alt="profile image"
+          className="w-30 h-30 rounded-full hover:scale-110 transition-transform duration-200 mb-4 sm:mb-0 flex-shrink-0"
+        />
+        <div className="sm:ml-6 text-center sm:text-left">
+          <h1 className="font-bold text-2xl sm:text-3xl mb-2">Alex Cottam</h1>
+          <p className="text-lg">
+            SDET 1 @{" "}
+            <a
+              className="text-amber-400 underline"
+              target="_blank"
+              href="https://www.verisk.com/"
+            >
+              Verisk
+            </a>
+          </p>
+          <p className="text-lg">
+            Web Developer @{" "}
+            <a
+              className="text-amber-400 underline"
+              target="_blank"
+              href="https://atonom.ai/"
+            >
+              Atonom
+            </a>
+          </p>
         </div>
-    );
-}
+      </div>
+      <hr className="border-t border-gray-300 my-4" />
+      <div className="mt-4">
+        <p className="text-lg mb-2 font-semibold">
+          Hi! I'm Alex, a Full-Stack Software Developer. I...
+        </p>
+        <ul className="list-disc list-inside mb-2">
+          <li className="text-sm">
+            Create user-friendly and performant mobile applications and websites
+          </li>
+          <li className="text-sm">
+            Develop and maintain RESTful APIs in Django and Node
+          </li>
+          <li className="text-sm">
+            Work with Google Cloud, Firebase, and Supabase
+          </li>
+          <li className="text-sm">
+            Architect and manage automated testing frameworks for UI, unit, and
+            API testing with tools such as Playwright and TestNG.
+          </li>
+        </ul>
+      </div>
+      <div className="mt-4">
+        <p className="text-lg mb-2 font-semibold">Other interests:</p>
+        <ul className="list-disc list-inside mb-2">
+          <li className="text-sm">Watch and play hockey (Go Mammoths!)</li>
+          <li className="text-sm">Backend development with Express.js</li>
+          <li className="text-sm">
+            Cook and eat delicious food from all cultures
+          </li>
+          <li className="text-sm">Read sci-fi and fantasy novels</li>
+          <li className="text-sm">Playing video games (especially Nintendo)</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,95 +1,108 @@
 const Experience = () => {
-    return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">Work Experience</h1>
-            <hr className="border-t border-gray-300 mb-4" />
-            <div className="mb-4">
-                <h2 className="text-xl font-bold mb-1">iOS Developer</h2>
-                <p className="text-gray-700">Amano Technologies - Provo, UT</p>
-                <p className="text-gray-600">Feb 2025 - Present</p>
-                <ul className="list-disc list-inside">
-                    <li className="text-sm">
-                        Designed and developed a comprehensive "Alerts" page
-                        with dynamic table views, service entry tracking, and
-                        tab bar badge integration, significantly improving user
-                        engagement and response times.
-                    </li>
-                    <li className="text-sm">
-                        Implemented a two-stage loading and caching mechanism,
-                        drastically reducing load times for dashboards and
-                        property details, and eliminating redundant network
-                        calls on the notifications page.
-                    </li>
-                    <li className="text-sm">
-                        Refactored the networking layer for enhanced resilience,
-                        incorporating advanced error handling, comprehensive
-                        logging, and improved user experience on slower
-                        connections via adjusted timeouts.
-                    </li>
-                    <li className="text-sm">
-                        Developed features and new endpoints for Amano's Node.js
-                        API, including new salt cell feature, integrating with a
-                        Firebase NoSQL database hosted on Google Cloud.
-                    </li>
-                </ul>
-            </div>
-            <div className="mb-4">
-                <h2 className="text-xl font-bold mb-1">Web Dev Lab Aide</h2>
-                <p className="text-gray-700">
-                    Salt Lake Community College - Taylorsville, UT
-                </p>
-                <p className="text-gray-600">Jan 2025 - Present</p>
-                <ul className="list-disc list-inside">
-                    <li className="text-sm">
-                        Provided comprehensive instructional support for web
-                        development courses, covering essential technologies
-                        such as HTML, CSS, JavaScript, WordPress, PHP, and
-                        Figma.
-                    </li>
-                    <li className="text-sm">
-                        Led engaging weekly lab sessions, guiding students
-                        through complex homework assignments and classwork,
-                        fostering a deeper understanding of web development
-                        principles and practical application.
-                    </li>
-                    <li className="text-sm">
-                        Developed and implemented detailed grading rubrics for
-                        coursework, ensuring fair and consistent evaluation of
-                        student performance and progress while providing
-                        constructive feedback.
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h2 className="text-xl font-bold mb-1">Web Developer</h2>
-                <p className="text-gray-700">
-                    ZwiCoat Materials Innovations - Remote
-                </p>
-                <p className="text-gray-600">Sept 2024 - May 2025</p>
-                <ul className="list-disc list-inside">
-                    <li className="text-sm">
-                        Spearheaded the end-to-end development and successful
-                        launch of Zwicoat's medical technology website,
-                        leveraging React, React Router, Tanstack Query, HTML,
-                        CSS, and JavaScript to deliver a modern and functional
-                        platform.
-                    </li>
-                    <li className="text-sm">
-                        Managed project timelines and maintained high code
-                        quality through diligent use of GitHub for version
-                        control and collaborative development, ensuring timely
-                        delivery and long-term maintainability.
-                    </li>
-                    <li className="text-sm">
-                        Engineered a fully responsive website layout,
-                        significantly improving user experience across all
-                        devices and optimizing for search engine discoverability
-                        (SEO) to increase online visibility.
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Work Experience</h1>
+      <hr className="border-t border-gray-300 mb-4" />
+      <div className="mb-4">
+        <h2 className="text-xl font-bold mb-1">SDET 1</h2>
+        <p className="text-gray-700">
+          Verisk Property Estimating Solutions (Xactware) - Lehi, UT
+        </p>
+        <p className="text-gray-600">Jan 2026 - Current</p>
+        <ul className="list-disc list-inside">
+          <li className="text-sm">
+            Engineered comprehensive automated testing solutions, including UI,
+            Unit, and Integration tests, within a custom Java automation
+            repository to ensure high quality delivery cycles.
+          </li>
+          <li className="text-sm">
+            Optimized test suite stability and performance by debugging legacy
+            Java/TestNG suites, reducing the failure rate by 74%.
+          </li>
+          <li className="text-sm">
+            Architected and spearheaded a greenfield migration from Java to a
+            modern TypeScript and Playwright framework, streamlining the
+            execution of E2E UI and API unit tests for better scalability and
+            stability.
+          </li>
+        </ul>
+      </div>
+      <div className="mb-4">
+        <h2 className="text-xl font-bold mb-1">Junior Web Developer</h2>
+        <p className="text-gray-700">Atonom AI - Provo, UT</p>
+        <p className="text-gray-600">Aug 2025 - Current</p>
+        <ul className="list-disc list-inside">
+          <li className="text-sm">
+            Collaborate closely with the marketing team to design and implement
+            clean, responsive web pages using React and TypeScript for front-end
+            components and Node.js and PostgreSQL for a backend API and
+            relational database.
+          </li>
+          <li className="text-sm">
+            Architect and deploy internal solutions, such as a customer issue
+            tracking tool and a CS team collaboration tool, using HTMX and
+            GoLang.
+          </li>
+          <li className="text-sm">
+            Create automation workflows with tools such as n8n and Make for
+            sales outreach, support ticket creation, and account executive
+            routing for clients.
+          </li>
+        </ul>
+      </div>
+      <div className="mb-4">
+        <h2 className="text-xl font-bold mb-1">
+          Full-Stack Software Developer
+        </h2>
+        <p className="text-gray-700">Amano Technologies - Provo, UT</p>
+        <p className="text-gray-600">Feb 2025 - Sept 2025</p>
+        <ul className="list-disc list-inside">
+          <li className="text-sm">
+            Led the design and deployment of a new alerts page for the iOS app,
+            boosting user engagement and improving response times for critical
+            notifications through Firebase Cloud Messaging.
+          </li>
+          <li className="text-sm">
+            Developed a two-stage caching system that reduced dashboard load
+            times in the customer app by 50%, improving overall user experience
+            and performance.
+          </li>
+          <li className="text-sm">
+            Implemented advanced error handling in the network layer, reducing
+            app crashes and increasing system resilience.
+          </li>
+          <li className="text-sm">
+            Authored and integrated new endpoints for a Node.js REST API,
+            supporting new features and seamless Firebase NoSQL database
+            connectivity.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="text-xl font-bold mb-1">Web Development Aide</h2>
+        <p className="text-gray-700">
+          Salt Lake Community College - Taylorsville, UT
+        </p>
+        <p className="text-gray-600">Feb 2025 - Feb 2026</p>
+        <ul className="list-disc list-inside">
+          <li className="text-sm">
+            Enhanced student learning outcomes by providing instructional
+            support for web development courses, covering HTML, CSS, JavaScript,
+            PHP, WordPress, and Figma.
+          </li>
+          <li className="text-sm">
+            Led weekly lab sessions, teaching students to apply knowledge to
+            practical projects.
+          </li>
+          <li className="text-sm">
+            Authored and implemented new grading rubrics and course materials,
+            streamlining evaluation and improving the curriculum for current and
+            future cohorts.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
